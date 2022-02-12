60a90102d0f9327835bb35d111a1634a56be224d
帆 <hehong_yf@163.com>
2022-02-12 21:06:55 +0800

    chore: 封装indexedDB

---
+++ packages/renderer/src/indexedDB.ts
@@ -0,0 +1,114 @@
+export default {
+    // 数据库
+    db:null,
+    indexedDB: window.indexedDB ||  window.webkitindexedDB || window.msIndexedDB || mozIndexedDB,
+    openDB(dbName:string,version=1){
+        return new Promise((resolve,reject)=>{
+            const request = this.indexedDB.open(dbName,version);
+            request.onerror = e => {
+                console.log('indexedDB open error',e);
+                reject(e);
+            };
+            request.onsuccess = e => {
+                console.log('indexedDB open success',e);
+                this.db = e.target.result;
+                resolve(request.result);
+            };
+            request.onupgradeneeded = e => {
+                console.log('indexedDB onupgradeneeded',e);
+                this.db = e.target.result;
+                const initDBs = ['like','album'];
+
+
+                // this.db.createObjectStore(objStoreName,{keyPath:'id'});
+
+                
+                for(let i = 0; i< initDBs.length; i++ ){
+                    if(!this.db.objectStoreNames.contains(initDBs[i])){
+                        let objectStore = this.db.createObjectStore(initDBs[i],{
+                            keyPath:'id',
+                            // autoIncrement:true
+                        });
+                        // 索引创建
+                    }
+                }
+                
+                resolve('');
+            };
+            // request.onupgradeneeded = e => {
+            //     console.log('indexedDB open onupgradeneeded',e);
+            //     this.db = e.target.result;
+            //     if(!this.db.objectStoreNames.contains(objStoreName)){
+            //         let objectStore = this.db.createObjectStore(objStoreName,{keyPath:'id',autoIncrement:true});
+            //     }
+            // }
+        });
+    },
+    deleteDB(dbName:string){
+        return new Promise((resolve,reject) => {
+            const request = this.indexedDB.deleteDatabase(dbName);
+            request.onsuccess = () =>resolve();
+            request.onerror = e =>reject(e);
+        });
+    },
+    closeDB(){
+        return new Promise((resolve,reject) =>{
+            if(!this.db) {
+                resolve()
+            }else{
+                this.db.close();
+                this.db = null;
+                resolve();
+            } 
+        });
+    },
+    add(objStoreName,arg){
+        console.log(this.db)
+        return new Promise((resolve,reject) =>{
+            if(!this.db) reject('数据库未连接！');
+            const request = this.db.transaction(objStoreName,'readwrite').objectStore(objStoreName).add(arg);
+            request.onsuccess = () => resolve(request.result);
+            request.onerror = e => reject(e);
+        });
+    },
+    get(objStoreName){
+        return new Promise((resolve,reject)=>{
+            if(!this.db) reject('数据库未连接');
+            const request = this.db.transaction([objStoreName],'readwrite').objectStore(objStoreName).getAll();
+            request.onerror = e => reject(e);
+            request.onsuccess = e => resolve(request.result);
+        });
+    },
+    forEach(objStoreName:string){
+        return new Promise((resolve,reject)=>{
+            if(!this.db) reject('数据库未连接');
+            const request = this.db.transaction([objStoreName],'readwrite').objectStore(objStoreName).openCursor();
+            request.onerror = e => reject(e);
+            request.onsuccess = () => resolve(request.result);
+        });
+    },
+    update(objStoreName,arg){
+        return new Promise((resolve,reject)=>{
+            if(!this.db) reject('数据库未连接');
+            const request = this.db.transaction([objStoreName],'readwrite').objectStore(objStoreName).put(arg);
+            request.onerror = e => reject(e);
+            request.onsuccess = () => resolve(request.result);
+        });
+    },
+    remove(objStoreName,index){
+        return new Promise((resolve,reject)=>{
+            if(!this.db) reject('数据库未连接');
+            const request = this.db.transaction([objStoreName],'readwrite').objectStore(objStoreName).delete(index);
+            request.onerror = e => reject(e);
+            request.onsuccess = () => resolve(request.result);
+        });
+    },
+    searchFromIndex(objStoreName,index,value){
+        return new Promise((resolve,reject)=>{
+            if(!this.db) reject('数据库未连接');
+            const request = this.db.transaction([objStoreName],'readwrite').objectStore(objStoreName).index(index).getAll(value);
+            request.onerror = e => reject(e);
+            request.onsuccess = () => resolve(request.result);
+        });
+    }
+};
--- packages/renderer/src/layouts/default-layout.vue
+++ packages/renderer/src/layouts/default-layout.vue
@@ -1,8 +1,4 @@
-<script lang="ts" setup>
-import layoutNav from '@/layouts/layout-nav.vue';
-import AudioPlayer from '@/layouts/audio-player.vue';
-import nSearch from '../components/n-search.vue';
-</script>
+
 <template>
   <div class="flex flex-col h-full overflow-hidden">
     <div
@@ -18,3 +14,48 @@ import nSearch from '../components/n-search.vue';
     <audio-player />
   </div>
 </template>
+<script lang="ts" setup>
+import layoutNav from '@/layouts/layout-nav.vue';
+import AudioPlayer from '@/layouts/audio-player.vue';
+import nSearch from '../components/n-search.vue';
+import indexedDB from '/@/indexedDB';
+import {ref} from 'vue';
+const dbOnline = ref(false); // 数据库是否在线;
+indexedDB.openDB('TIMP',1).then(()=>{
+  dbOnline.value = true;
+  console.log('数据库在线');
+});
+
+
+
+
+// 从数据库获取数据
+
+// 如果不存在"我喜欢"歌单，创建
+
+// 监听添加歌曲到歌单事件 
+
+
+// 监听从歌单删除歌曲事件《仅自建歌单有此功能》
+
+// 监听收藏歌单事件 【将歌单信息存入数据库: 歌单id,歌单名称】
+
+// 监听删除歌单事件 [分自建歌单与网路歌单]
+
+setTimeout(()=>{
+  // indexedDB.add('music', {
+  //   id:789,
+  //   name:"我喜欢",
+  //   list:[123,2999],
+  // }).then(res=>{
+  //   console.log(res);
+  // });
+  indexedDB.get('like').then(res=>{
+    console.log(res);
+  });
+},1000);
+
+// 构建本地歌单
+
+
+</script>
